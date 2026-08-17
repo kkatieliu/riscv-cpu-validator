ae9c643cf1331c31ed28934d82d0a998341d1aac
Katherine Liu <katherinehelen.liu@mail.utoronto.ca>
2026-08-17 15:41:54 +0800

    add cpu.sv

---
+++ rtl/cpu.sv
@@ -0,0 +1,77 @@
+module cpu (
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [31:0] instruction,
+    output logic [31:0] result
+);
+// cpu is the highest level module so it needs to speak to other parts of the system
+// specifically it communicates with RAM to get the instruction and then stores the output result into RAM as well 
+// rst is controlled externally 
+// clk comes from the clock generator which is a dediacted circuits whos only job is to perfectly ossilate 0 and 1 
+// the cpu doesnt need to have input and output bcuz these are internal connections , nothing outside the cpu sees them 
+// q: how does cpu.sv talk to alu.sv? a: at compile time they all compile together 
+
+    
+
+    // IF/ID signals
+    logic [31:0] if_pc;
+    logic [31:0] id_pc;
+    logic [31:0] id_instruction;
+
+    // ID/EX signals
+    logic [31:0] id_rs1_data;
+    logic [31:0] id_rs2_data;
+    logic [31:0] id_immediate;
+    logic [4:0]  id_rd;
+    logic [3:0]  id_alu_op;
+
+    // EX signals
+    logic [31:0] ex_rs1_data;
+    logic [31:0] ex_rs2_data;
+    logic [31:0] ex_immediate;
+    logic [4:0]  ex_rd;
+    logic [3:0]  ex_alu_op;
+    logic [31:0] ex_pc;
+
+    // ALU signals 
+    logic zero;
+
+    // in a real CPU the pc is generated itself 
+    // in this case we will hard code it as 0 first so we can test it for now. 
+    assign if_pc = 32'b0;
+
+    if_id_reg if_id(
+        .clk(clk), 
+        .rst(rst), 
+        .if_pc(if_pc), 
+        .if_instruction(instruction), 
+        .id_pc(id_pc), 
+        .id_instruction(id_instruction)
+    );
+
+    id_ex_reg id_ex(
+        .clk(clk),
+        .rst(rst),
+        .id_pc(id_pc),
+        .id_rs1_data(id_rs1_data),
+        .id_rs2_data(id_rs2_data),
+        .id_immediate(id_immediate),
+        .id_rd(id_rd),
+        .id_alu_op(id_alu_op),
+        .ex_pc(ex_pc),
+        .ex_rs1_data(ex_rs1_data),
+        .ex_rs2_data(ex_rs2_data),
+        .ex_immediate(ex_immediate),
+        .ex_rd(ex_rd),
+        .ex_alu_op(ex_alu_op)
+    );
+
+    alu alu_inst(
+        .operand_a(ex_rs1_data), 
+        .operand_b(ex_rs2_data), 
+        .alu_op(ex_alu_op), 
+        .result(result), 
+        .zero(zero)
+    );
+
+endmodule
--- rtl/pipeline_regs.sv
+++ rtl/pipeline_regs.sv
@@ -13,7 +13,7 @@ module if_id_reg(
 
 always_ff @(posedge clk) begin
     if (rst) begin 
-        id_pc <= 32'b0;~
+        id_pc <= 32'b0;
         id_instruction <= 32'b0; 
     end else begin 
         id_pc <= if_pc;
@@ -21,4 +21,44 @@ always_ff @(posedge clk) begin
     end 
 end
 
+
 endmodule
+
+// id immidiate is for if we want to add a constant value 
+// there are 32 registers x0 to x31! completely seperate from RAM, but also a storage space for data
+module id_ex_reg (
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [31:0] id_pc,
+    input  logic [31:0] id_rs1_data,
+    input  logic [31:0] id_rs2_data,
+    input  logic [31:0] id_immediate,
+    input  logic [4:0]  id_rd,
+    input  logic [3:0]  id_alu_op,
+    output logic [31:0] ex_pc,
+    output logic [31:0] ex_rs1_data,
+    output logic [31:0] ex_rs2_data,
+    output logic [31:0] ex_immediate,
+    output logic [4:0]  ex_rd,
+    output logic [3:0]  ex_alu_op
+);
+
+always_ff @(posedge clk) begin
+    if (rst) begin 
+        ex_pc <= 32'b0;
+        ex_rs1_data <= 32'b0;
+        ex_rs2_data <= 32'b0;
+        ex_immediate <= 32'b0;
+        ex_rd <= 5'b0;
+        ex_alu_op <= 4'b0;
+    end else begin
+        ex_pc <= id_pc;
+        ex_rs1_data <= id_rs1_data;
+        ex_rs2_data <= id_rs2_data;
+        ex_immediate <= id_immediate;
+        ex_rd <= id_rd;
+        ex_alu_op <= id_alu_op;
+    end 
+end 
+
+endmodule 
